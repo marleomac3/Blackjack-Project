d4e713be27b0a512292f8677ddcfb3bb68eedac2
Marion McWilliams <marionmcwilliams@Marions-MacBook-Air.local>
2025-02-25 11:52:41 -0500

    Created passing tests for creating deck and shuffling deck

---
+++ game/deck.py
@@ -0,0 +1,13 @@
+class Deck:
+    def __init__(self):
+        self.cards = []
+        self.create_deck()
+
+    def create_deck(self):
+        suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+        ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+        self.cards = [(rank, suit) for rank in ranks for suit in suits] # This line creates a list of tuples, where each tuple represents a card in the deck.
+
+    def shuffle(self):
+        import random
+        random.shuffle(self.cards)
